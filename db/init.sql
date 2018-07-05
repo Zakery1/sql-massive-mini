e78dcb43502d8d69114ba5aa7faf68af245a79b9
Zachary Graham <zakgraham@gmail.com>
2018-07-05 12:03:12 -0700

    add server

--- db/init.sql
+++ db/init.sql
@@ -9,5 +9,6 @@ CREATE TABLE heroes (
     picture_url text
 );
 
-insert into heroes (name, street_name, age, powers, picture_url) values ('Batman', 'Bruce', 45, 'super-rich, tool-belt, cool-buttler', 'https://www.lego.com/en-us/batmanmovie/characters')
-
+insert into heroes (name, street_name, age, powers, picture_url) values ('Batman', 'Bruce', 45, 'super-rich, tool-belt, cool-buttler', 'https://www.lego.com/en-us/batmanmovie/characters');
+INSERT INTO heroes (name, street_name, age, powers, picture_url) values ('Homer', 'Springfield', 30, 'unlimited hunger and thirst, mediocre dad, nuclear plant safety dude', 'http://cdn.skim.gs/images/homer-simpson-doughnuts/what-homer-simpson-taught-us-about-doughnuts
+')
